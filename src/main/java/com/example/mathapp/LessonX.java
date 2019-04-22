f575f17ab7082e5f2055428e57c31ea0dced63e5
Sanchita Syeda <sanchitasyeda@gmail.com>
2019-04-21 21:36:30 -0400

    Adding new files for math application

---
+++ src/main/java/com/example/mathapp/LessonX.java
@@ -0,0 +1,14 @@
+package com.example.mathapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LessonX extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson_x);
+    }
+}
